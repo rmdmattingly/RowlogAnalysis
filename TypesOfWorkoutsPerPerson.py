2f42f4fbc82ccd0cd188b225a3e38f975847feb2
Tadd Bindas <Tadd.Bindas@ibm.com>
2019-12-05 17:05:09 -0500

    Types of workouts function complete

--- TypesOfWorkoutsPerPerson.py
+++ TypesOfWorkoutsPerPerson.py
@@ -1,18 +1,14 @@
 import requests
-import collections
-from collections import defaultdict
 from pprint import pprint
-import enum
+from enum import Enum
 
-class WorkoutTypes(enum.Enum):
+class WorkoutTypes(Enum):
     Erg = 0
     Bike = 1
     Run = 2
     Swim = 3
 
 ### useful functions ###
-def sortDictionaryByValues(dictionary):
-    return sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True)
 
 def getData(url):
     response = requests.get(url)
@@ -20,20 +16,18 @@ def getData(url):
 
 def getTypesOfWorkoutsPerPerson(workoutsData):
     typesOfWorkoutsPerPerson = {}
+
     for workout in workoutsData:
         name = workout['name']
         type = workout['type']
         if name not in typesOfWorkoutsPerPerson:
-            typesOfWorkoutsPerPerson[name] = list(range(0,3))
+            typesOfWorkoutsPerPerson[name] = {}
         for activity in WorkoutTypes:
-            if activity not in typesOfWorkoutsPerPerson[name]:
-                typesOfWorkoutsPerPerson[name][activity.value] = 0
-            if type in activity:
-                typesOfWorkoutsPerPerson[name][activity.value] += 1
-
-    return sortDictionaryByValues(workoutsPerPerson)
-
-    return sortDictionaryByValues(typesOfWorkoutsPerPerson)
+            if activity.name not in typesOfWorkoutsPerPerson[name]:
+                typesOfWorkoutsPerPerson[name][activity.name] = 0
+            if type in activity.name:
+                typesOfWorkoutsPerPerson[name][activity.name] += 1
+    return typesOfWorkoutsPerPerson
 
 
 
