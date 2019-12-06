b353de24df207ce7677d930c9c4732bb66ee81e8
Logan Farmer <logan.farmer1@marist.edu>
2019-12-06 13:09:07 -0500

    Create totalStarboardMeters.py

---
+++ totalStarboardMeters.py
@@ -0,0 +1,6 @@
+def getStarboardMeters(workoutsData, peopleData):
+    starboardMeters = 0
+    for workout in workoutsData:
+        if workout.person.side = starboard
+            starboardMeters += workout['scored_meters']
+    return starBoardMeters
