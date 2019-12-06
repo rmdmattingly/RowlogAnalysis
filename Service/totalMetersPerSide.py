2fd27a72511ef1cef59bd0c1074c4e2cb3e77375
Logan Farmer <logan.farmer1@marist.edu>
2019-12-06 13:11:36 -0500

    added port side (gross)

---
+++ Service/totalMetersPerSide.py
@@ -0,0 +1,15 @@
+def getStarboardMeters(workoutsData, peopleData):
+    starboardMeters = 0
+    for workout in workoutsData:
+        if workout.person.side = 'starboard'
+            starboardMeters += workout['scored_meters']
+    return starBoardMeters
+
+
+
+def getPortMeters(workoutsData, peopleData):
+    portMeters = 0
+    for workout in workoutsData:
+        if workout.person.side = 'port'
+            portMeters += workout['scored_meters']
+    return portMeters
--- totalStarboardMeters.py
+++
@@ -1,6 +0,0 @@
-def getStarboardMeters(workoutsData, peopleData):
-    starboardMeters = 0
-    for workout in workoutsData:
-        if workout.person.side = starboard
-            starboardMeters += workout['scored_meters']
-    return starBoardMeters
