44ca00d59467cb69835805f38ca06d336d6da400
Ray Mattingly <rmattingly@hubspot.com>
2019-12-07 13:41:51 -0500

    adjusting comment styling

--- Core/SplitManager.py
+++ Core/SplitManager.py
@@ -6,12 +6,13 @@ def calcSeconds(min, sec):
 def parseStringToMinAndSec(splitTime):
     minsAndSecArr = splitTime.split(':')
     return minsAndSecArr[0], minsAndSecArr[1]
-
-def convertSplitToSeconds(splitTime): # call this function to convert a split like 1:30.6 to seconds 90.6 -- makes it easier to rank by split
+# call this function to convert a split like 1:30.6 to seconds 90.6 -- makes it easier to rank by split
+def convertSplitToSeconds(splitTime):
     min, sec = parseStringToMinAndSec(splitTime)
     return calcSeconds(min, sec)
 
-def convertSecondsToSplit(sec): # call this function to convert seconds like 95.6 to a split 1:35.6 -- makes it easier to display data
+# call this function to convert seconds like 95.6 to a split 1:35.6 -- makes it easier to display data
+def convertSecondsToSplit(sec):
     min = int(math.floor(float(sec) / 60))
     sec = float(sec) - (min*60)
     return str(min) + ':' + str(sec)
