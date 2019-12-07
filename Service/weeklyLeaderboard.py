36d660b2c7c40f1750119f67d493eed9fbb241e4
Jordan Irving <20100220@marist.edu>
2019-12-06 21:22:51 -0500

    Create weeklyLeaderboard.py

---
+++ Service/weeklyLeaderboard.py
@@ -0,0 +1,137 @@
+from datetime import timedelta, date
+    
+def week1range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 1')
+start_dt = date(2019, 12, 8)
+end_dt = date(2019, 12, 14)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week1rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week1range
+            meters += workout['scored_meters']
+    return meters 
+
+
+def week2range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 2')
+start_dt = date(2019, 12, 15)
+end_dt = date(2019, 12, 21)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+    
+def week2rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week2range
+            meters += workout['scored_meters']
+    return meters 
+
+
+
+def week3range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 3')
+start_dt = date(2019, 12, 22)
+end_dt = date(2019, 12, 28)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week3rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week3range
+            meters += workout['scored_meters']
+    return meters 
+
+
+
+
+def week4range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 4')
+start_dt = date(2019, 12, 29)
+end_dt = date(2020, 1, 4)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week4rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week4range
+            meters += workout['scored_meters']
+    return meters 
+
+
+
+
+
+
+def week5range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 5')
+start_dt = date(2020, 1, 5)
+end_dt = date(2020, 1, 11)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week5rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week5range
+            meters += workout['scored_meters']
+    return meters 
+
+
+
+
+
+def week6range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 6')
+start_dt = date(2020, 1, 12)
+end_dt = date(2020, 1, 18)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week6rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week6range
+            meters += workout['scored_meters']
+    return meters 
+
+
+
+def week7range(date1, date2):
+     for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+print('week 7')
+start_dt = date(2020, 1, 19)
+end_dt = date(2020, 1, 25)
+for dt in week1range(start_dt, end_dt):
+    print(dt.strftime("%Y-%m-%d"))
+
+def week7rangehelper(workoutsData, peopleData, time): 
+    i = people.length
+    for workout in workoutsData:
+        if workout.person.time in week7range
+            meters += workout['scored_meters']
+    return meters 
