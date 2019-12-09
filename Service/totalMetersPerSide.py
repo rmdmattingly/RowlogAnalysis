d45e3e318352a97a4875b702a6c2e7f61fb71380
Logar18 <42846966+Logar18@users.noreply.github.com>
2019-12-09 15:35:42 -0500

    changed to one function

--- Service/totalMetersPerSide.py
+++ Service/totalMetersPerSide.py
@@ -1,15 +1,15 @@
-def getStarboardMeters(workoutsData, peopleData):
-    starboardMeters = 0
-    for workout in workoutsData:
-        if workout.person.side = 'starboard'
-            starboardMeters += workout['scored_meters']
-    return starBoardMeters
+def getSideMeters(workoutData, peopleData, side):
+    sideMeters = 0
+    for workout in workoutData
+        if workout.person.side = side
+            sideMeters += workout['scored_meters']
+        return sideMeters
 
 
+def getPortMeters(workoutData, peopleData):
+    return getSideMeters(workoutData, peopleData, 'port')
 
-def getPortMeters(workoutsData, peopleData):
-    portMeters = 0
-    for workout in workoutsData:
-        if workout.person.side = 'port'
-            portMeters += workout['scored_meters']
-    return portMeters
+
+def getStarboardMeters(workoutData, peopleData):
+    return getSideMeters(workoutData, peopleData, 'starboard')
+
