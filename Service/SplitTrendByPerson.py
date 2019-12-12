c1a5411db28b2d83cd04a2f831d496c84c618da5
Logar18 <42846966+Logar18@users.noreply.github.com>
2019-12-12 16:12:17 -0500

    Update SplitTrendByPerson.py

--- Service/SplitTrendByPerson.py
+++ Service/SplitTrendByPerson.py
@@ -11,17 +11,19 @@ def findSplitTrendsByPerson(workoutData, people_id, SplitManager, DateManager):
         if workout['people_id'] == people_id and workout['type'] == 'Erg':
             workoutDate = DateManager.fetchDateFromTimestampString(workout['time'])
             if workout['intensity'] == 'U1':
-                u1Data[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                u1Data[workoutDate] = workout['avg_split']
             elif workout['intensity'] == 'U2':
-                u2Data[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                u2Data[workoutDate] = workout['avg_split']
             elif workout['intensity'] == 'U3':
-                u3Data[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                u3Data[workoutDate] = workout['avg_split']
             elif workout['intensity'] == 'AT':
-                ATData[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                ATData[workoutDate] = workout['avg_split']
             elif workout['intensity'] == 'TR':
-                TRData[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                TRData[workoutDate] = workout['avg_split']
             elif workout['intensity'] == 'Test':
-                testData[workoutDate] = SplitManager.parseStringToMinAndSec(workout['avg_split'])
+                testData[workoutDate] = workout['avg_split']
+            elif workout['intensity'] is None:
+                pass
             else:
                 print("Something went wrong.")
     splitTrendData['U1'] = u1Data
