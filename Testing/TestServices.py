c9d3ef57b55440237809a40f38628f30f173fa67
Tadd-Bindas <Tadd.Bindas@ibm.com>
2019-12-14 12:07:13 -0800

    Made a pytest script. Still running through bugs

--- Testing/TestServices.py
+++ Testing/TestServices.py
@@ -1,9 +1,36 @@
-import os
 import pytest
-from ... import main
 
-def testService(service, arg):
-    print(os.system('python ../main.py service'))
+@pytest.fixture
+def serviceCalls():
+    import json, os
+    from jsonschema import validate
 
-for service in main.switcher:
-    testService(service)
+    python = "python ~/RowlogAnalysis/main.py"
+
+    def serviceCalls(service, args):
+        print(service)
+        output = os.system(python + " " + service + " " + args)
+        if service in "Invalid Service":
+            assert isinstance(json, str)
+        else:
+            testCalls(output)
+
+    def testCalls(output):
+        assert validate(instance=json.loads(str(output)), schema=output)
+
+    yield serviceCalls
+
+def testServices(serviceCalls):
+    from RowlogAnalysis import main
+    from .MultipleArgScripts import MultipleArgScripts
+
+    hasOneArg = True
+
+    for service in main.switcher:
+        for script in MultipleArgScripts:
+            if service in script.name:
+                serviceCalls(service, script.value)
+                hasOneArg = False
+        if hasOneArg:
+            serviceCalls(service, "")
+        hasOneArg = True
