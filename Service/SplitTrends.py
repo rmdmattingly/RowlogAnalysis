df3537a88fb69daa4cc4461e619ce3ae5ffdd09c
Logar18 <lfarmera2@gmail.com>
2019-12-16 13:30:53 -0500

    added workoutCount

--- Service/SplitTrends.py
+++ Service/SplitTrends.py
@@ -19,19 +19,25 @@ def findAverageSplitByDate(date, workoutData, people_id, SplitManager, intensity
 def findSplitTrends(workoutData, SplitManager, DateManager):
     orderedWorkoutData = DateManager.getWorkoutsDataChronologically(workoutData)
     splitTrendData = {}
+    workoutCount = {}
     for workout in orderedWorkoutData:
         intensity = workout['intensity']
         if isValidIntensity(intensity) and isValidActivity(workout['type']):
             name = workout['name']
             if name not in splitTrendData.keys():
                 splitTrendData[name] = {}
+                workoutCount[name] = {}
             date = DateManager.fetchDateFromTimestampString(workout['time'])
             if date not in splitTrendData[name]:
                 splitTrendData[name][date] = {}
                 splitTrendData[name][date][intensity] = 0
+                workoutCount[name][date] = {}
+                workoutCount[name][date][intensity] = 0
             if intensity not in splitTrendData[name][date]:
                 splitTrendData[name][date][intensity] = 0
+                workoutCount[name][date][intensity] = 0
             splitTrendData[name][date][intensity] += SplitManager.convertSplitToSeconds(workout['avg_split'])
+            workoutCount[name][date][intensity] += 1
     averageData = {}
     return splitTrendData
 
