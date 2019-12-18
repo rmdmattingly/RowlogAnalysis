7f36ab29d464e6cfab3a2c7bce8e0cee70df6e93
Christopher Crawley <31451256+cc12698@users.noreply.github.com>
2019-12-18 14:53:39 -0500

    Percent of meters fix (#42)
    * added PrecentOFMeters
    
    * branch up to date
    
    * added Number of workouts and the precentage variables into myDict
    
    * progress
    
    * retrieved everyones individual activities successfully
    
    * percentages work
    
    * rounding to the hundredths
    
    * calcPerecentOfWork created and active var made
    
    * fixed an error that caused the wrong percentage to output
    
    * cleaned up

--- Service/PercentOfMeters.py
+++ Service/PercentOfMeters.py
@@ -1,13 +1,9 @@
-def getNameToDataDict(peopleData):
-    nameToData = {}
-    for ppl in peopleData:
-        nameToData[ppl['name']] = ppl
-    return nameToData
-
 def calcPercentOfWork(numOfWork, name, activity):
-    return round(100 * float(numOfWork[name][activity.name]['NumberOfWorkouts'])/float(numOfWork[name]['total']), 2)
+    num = numOfWork[name][activity.name]['NumberOfWorkouts']
+    tot = numOfWork[name]['total']
+    return round((100 * float(num)/float(tot)), 2)
 
-def getDisplay(workoutsData, Activites, nameToData):
+def getDisplay(workoutsData, Activites):
     numOfWork = {}
     for workout in workoutsData:
         type = workout['type']
@@ -18,20 +14,19 @@ def getDisplay(workoutsData, Activites, nameToData):
         numOfWork[name]['total'] += 1
         for activity in Activites:
             active = activity.name
-            if (active not in numOfWork[name].keys()):
+            if active not in numOfWork[name].keys():
                 numOfWork[name][active] = {}
                 numOfWork[name][active]['NumberOfWorkouts'] = 0
                 numOfWork[name][active]['PercentageOfWork'] = 0
             if type in active:
                 numOfWork[name][active]['NumberOfWorkouts'] += 1
-                numOfWork[name][active]['PercentageOfWork'] = calcPercentOfWork(numOfWork, name, activity)
+            numOfWork[name][active]['PercentageOfWork'] = calcPercentOfWork(numOfWork, name, activity)
     return numOfWork
 
-def percentOfMeters(peopleData, getWorkoutData, Activities):
-    nameToData = getNameToDataDict(peopleData)
+def percentOfMeters(getWorkoutData, Activities):
     myDict = {}
-    myDict['name'] = getDisplay(getWorkoutData, Activities, nameToData)
+    myDict['name'] = getDisplay(getWorkoutData, Activities)
     return myDict
 
-def run(peopleData, getWorkoutData, Activities):
-    return percentOfMeters(peopleData, getWorkoutData, Activities)
+def run(getWorkoutData, Activities):
+    return percentOfMeters(getWorkoutData, Activities)
