4c6ce3a5d312c4ac5d95b1f3de0c0bdf519c00ee
Ray Mattingly <rmattingly@hubspot.com>
2019-12-27 16:49:41 -0500

    migrating to rows.tech

--- Data/RowlogApi.py
+++ Data/RowlogApi.py
@@ -2,11 +2,11 @@ import requests
 
 # accepts orderyBy = time, real_meters, scored_meters, or avg_heartrate.  Only returns desc, reverse the response for asc (see: Service.ErgMetersPerDay.py)
 def getWorkoutData(teamCode, orderBy, comment):
-    url = 'https://quikfo.com/rowlog/api/workouts?teamCode=' + teamCode + '&orderBy=' + orderBy + '&comment=' + comment
+    url = 'https://rows.tech/api/workouts?teamCode=' + teamCode + '&orderBy=' + orderBy + '&comment=' + comment
     response = requests.get(url)
     return response.json()
 
 def getPeopleData(teamCode):
-    url = 'https://quikfo.com/rowlog/api/people?teamCode=' + teamCode
+    url = 'https://rows.tech/api/people?teamCode=' + teamCode
     response = requests.get(url)
     return response.json()
