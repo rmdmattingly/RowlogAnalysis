5c7e58b358e04074121a5a373100ed86d8e89e1b
Ray Mattingly <rmattingly@hubspot.com>
2020-02-20 17:55:09 -0500

    yuh

---
+++ Core/BoatManager.py
@@ -0,0 +1,27 @@
+def getBoats(peopleData):
+    boats = set()
+    for person in peopleData:
+        if person['boating'] not in boats:
+            boats.add(person['boating'])
+    return boats
+
+def findRowersInBoat(peopleData, boating):
+    peopleInBoat = set()
+    for person in peopleData:
+        if person['boating'] == boating and boating not in peopleInBoat:
+            peopleInBoat.add(person['people_id'])
+    return peopleInBoat
+
+def getAllPeopleDataForBoat(peopleData, boating):
+    output = []
+    for person in peopleData:
+        if person['boating'] == boating:
+            output.append(person)
+    return output
+
+def findNumRowersInBoat(peopleData, boating):
+    rowerCount = 0
+    for person in peopleData:
+        if person['boating'] == boating:
+            rowerCount += 1
+    return rowerCount
